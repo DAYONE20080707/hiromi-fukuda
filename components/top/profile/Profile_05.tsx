a745033c4b61df5fbf55c4da7b48dddbdafd869a
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-12-08 14:23:03 +0900

    Google MapをBunkamuraに

--- components/top/profile/Profile_05.tsx
+++ components/top/profile/Profile_05.tsx
@@ -1,12 +1,12 @@
 // components/profile/Profile_05.tsx
-"use client"
+"use client";
 
-import { useState, useEffect } from "react"
-import Image from "next/image"
-import Link from "next/link"
-import ContentHeadline from "@/components/ui/frame/ContentHeadline"
-import ProfileCard from "@/components/ui/ItemCard/ProfileCard_02"
-import SectionContent from "@/components/ui/frame/SectionContent"
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import ContentHeadline from "@/components/ui/frame/ContentHeadline";
+import ProfileCard from "@/components/ui/ItemCard/ProfileCard_02";
+import SectionContent from "@/components/ui/frame/SectionContent";
 
 const Profile_05 = () => {
   return (
@@ -22,7 +22,7 @@ const Profile_05 = () => {
           <div className="flex flex-col-reverse md:flex-row md:justify-center gap-6 md:gap-20 w-full">
             <div className="md:w-1/2 rounded-[20px] overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.6554893109096!2d139.6901269764477!3d35.66085872259378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cab61568af5%3A0x277db4a65337244d!2z44CSMTUwLTAwNDYg5p2x5Lqs6YO95riL6LC35Yy65p2-5r-k77yR5LiB55uu!5e0!3m2!1sja!2sjp!4v1764637365889!5m2!1sja!2sjp"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1516.9277173610326!2d139.69501979948862!3d35.66083898456399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cabd772ee75%3A0x62fdbb6826cbf539!2z44CSMTUwLTAwNDMg5p2x5Lqs6YO95riL6LC35Yy66YGT546E5Z2C77yS5LiB55uu77yS77yU4oiS77yR!5e0!3m2!1sja!2sjp!4v1765171290611!5m2!1sja!2sjp"
                 width="100%"
                 height="546"
                 style={{ border: "0" }}
@@ -34,13 +34,16 @@ const Profile_05 = () => {
             <div className="mt-8 md:mt-0 md:w-1/2">
               <div className="space-y-4">
                 <h3 className="text-accentColor text-base font-medium ![line-height:160%]">
-                福田ひろみバイオリン教室
+                  福田ひろみバイオリン教室
                 </h3>
                 <ProfileCard
                   label=""
                   value="〒150-0046　東京都渋谷区松濤1 (Bunkamuraから徒歩1分)"
                 />
-                <ProfileCard label="メールアドレス" value="lncjgi9606@yahoo.co.jp" />
+                <ProfileCard
+                  label="メールアドレス"
+                  value="lncjgi9606@yahoo.co.jp"
+                />
               </div>
 
               {/* 診療時間表 */}
@@ -144,7 +147,7 @@ const Profile_05 = () => {
         </section>
       </SectionContent>
     </>
-  )
-}
+  );
+};
 
-export default Profile_05
+export default Profile_05;
